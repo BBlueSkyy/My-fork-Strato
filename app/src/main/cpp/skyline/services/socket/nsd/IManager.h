98172b3ed1631b57fbe973012928b3f78d35ae17
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-07-20 00:59:29 -0300

    Add GetEnvironmentIdentifier function to IManager

--- app/src/main/cpp/skyline/services/socket/nsd/IManager.h
+++ app/src/main/cpp/skyline/services/socket/nsd/IManager.h
@@ -14,9 +14,11 @@ namespace skyline::service::socket {
         IManager(const DeviceState &state, ServiceManager &manager);
 
         Result ResolveEx(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetEnvironmentIdentifier(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
             SFUNC(0x15, IManager, ResolveEx),
+            SFUNC(0xB, IManager, GetEnvironmentIdentifier),
         )
     };
 }
