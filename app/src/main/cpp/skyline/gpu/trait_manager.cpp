c3d9a51276df88528e281ecf2a3873b08cb1d364
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-07-21 18:04:31 -0300

    Enable brokenTextureShadowCompare for multiple drivers

--- app/src/main/cpp/skyline/gpu/trait_manager.cpp
+++ app/src/main/cpp/skyline/gpu/trait_manager.cpp
@@ -250,6 +250,7 @@ namespace skyline::gpu {
 
                 brokenSubgroupShuffle = true;
                 brokenSpirvVectorAccessChain = true;
+                brokenTextureShadowCompare = true;
                 maxGlobalPriority = vk::QueueGlobalPriorityEXT::eHigh;
                 break;
             }
@@ -257,6 +258,7 @@ namespace skyline::gpu {
             case vk::DriverId::eMesaTurnip: {
                 vkImageMutableFormatCostly = true; // Disables UBWC and forces linear tiling
                 adrenoRelaxedFormatAliasing = true;
+                brokenTextureShadowCompare = true;  
                 break;
             }
 
