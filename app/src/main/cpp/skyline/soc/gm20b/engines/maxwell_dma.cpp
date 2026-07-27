a3c82c29621358f696f51fac090a291cd7554c33
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-07-27 11:39:01 -0300

    Fix copyCache resize to include offset (#8)
    Adjust the copyCache resize to account for offset when the destination mappings are not singular.

--- app/src/main/cpp/skyline/soc/gm20b/engines/maxwell_dma.cpp
+++ app/src/main/cpp/skyline/soc/gm20b/engines/maxwell_dma.cpp
@@ -104,7 +104,7 @@ namespace skyline::soc::gm20b::engine {
             size_t offset{isSrcSplit ? srcSize : 0};
 
             if (copyCache.size() < (dstSize + offset))
-                copyCache.resize(dstSize);
+            copyCache.resize(dstSize + offset);
 
             dst = copyCache.data() + offset;
 
