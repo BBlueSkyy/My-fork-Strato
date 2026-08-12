599baf1580a029a5292fa3a01b0afa10e2439232
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-08-11 21:31:16 -0300

    Add SetGestureOutputRanges function to IHidServer

--- app/src/main/cpp/skyline/services/hid/IHidServer.h
+++ app/src/main/cpp/skyline/services/hid/IHidServer.h
@@ -79,7 +79,12 @@ namespace skyline::service::hid {
          * @url https://switchbrew.org/wiki/HID_services#IsSixAxisSensorAtRest
          */
         Result IsSixAxisSensorAtRest(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
+      
+        /**
+         * @brief Sets the coordinate ranges used for touch gesture reporting (stub)
+         * @url https://switchbrew.org/wiki/HID_services#hid (cmd 92, [18.0.0+], sem doc oficial)
+         */
+        Result SetGestureOutputRanges(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         /**
          * @brief Sets the style of controllers supported
          * @url https://switchbrew.org/wiki/HID_services#SetSupportedNpadStyleSet
@@ -250,6 +255,7 @@ namespace skyline::service::hid {
             SFUNC(0x50, IHidServer, GetGyroscopeZeroDriftMode),
             SFUNC(0x51, IHidServer, ResetGyroscopeZeroDriftMode),
             SFUNC(0x52, IHidServer, IsSixAxisSensorAtRest),
+            SFUNC(0x5C, IHidServer, SetGestureOutputRanges),
             SFUNC(0x64, IHidServer, SetSupportedNpadStyleSet),
             SFUNC(0x65, IHidServer, GetSupportedNpadStyleSet),
             SFUNC(0x66, IHidServer, SetSupportedNpadIdType),
