1abf696c2cbdb305a7abef7c440ef484ea3f9704
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-08-25 17:12:49 -0300

    Update patch_manager.cpp

--- app/src/main/cpp/skyline/vfs/patch_manager.cpp
+++ app/src/main/cpp/skyline/vfs/patch_manager.cpp
@@ -10,12 +10,17 @@ namespace skyline::vfs {
     PatchManager::PatchManager() {}
 
     std::shared_ptr<FileSystem> PatchManager::PatchExeFS(const DeviceState &state, std::shared_ptr<FileSystem> exefs) {
+        if (!state.updateLoader || !state.updateLoader->programNca || !state.updateLoader->programNca->exeFs)
+            throw exception("Update does not contain a usable ExeFS");
         auto updateProgramNCA{state.updateLoader->programNca};
         return updateProgramNCA->exeFs;
     }
 
     std::shared_ptr<vfs::Backing> PatchManager::PatchRomFS(const DeviceState &state, std::optional<vfs::NCA> nca, u64 ivfcOffset) {
-        auto newNca{std::make_shared<vfs::NCA>(nca, state.os->keyStore, state.loader->programNca->romFs, ivfcOffset)};
+        if (!nca || !state.loader || !state.loader->programNca || !state.loader->programNca->rawRomFs)
+            throw exception("Cannot patch RomFS without update and base raw RomFS layers");
+
+        auto newNca{std::make_shared<vfs::NCA>(std::move(nca), state.os->keyStore, state.loader->programNca->rawRomFs, ivfcOffset)};
         return newNca->romFs;
     }
 }
