1132f03e400f8d7e44f0c0c8f9e92a27c9668180
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-08-25 22:42:35 -0300

    Update svc.cpp

--- app/src/main/cpp/skyline/kernel/svc.cpp
+++ app/src/main/cpp/skyline/kernel/svc.cpp
@@ -364,6 +364,7 @@ namespace skyline::kernel::svc {
 
         i64 in{static_cast<i64>(ctx.x0)};
         if (in > 0) {
+            LOGD("Sleeping for {}ns", in);
             TRACE_EVENT("kernel", "SleepThread", "duration", in);
 
             struct timespec spec{
@@ -376,6 +377,7 @@ namespace skyline::kernel::svc {
         } else {
             switch (in) {
                 case yieldWithCoreMigration: {
+                    LOGD("Waking any appropriate parked threads and yielding");
                     TRACE_EVENT("kernel", "YieldWithCoreMigration");
                     state.scheduler->WakeParkedThread();
                     state.scheduler->Rotate();
@@ -384,6 +386,7 @@ namespace skyline::kernel::svc {
                 }
 
                 case yieldWithoutCoreMigration: {
+                    LOGD("Cooperative yield");
                     TRACE_EVENT("kernel", "YieldWithoutCoreMigration");
                     state.scheduler->Rotate();
                     state.scheduler->WaitSchedule();
@@ -391,6 +394,7 @@ namespace skyline::kernel::svc {
                 }
 
                 case yieldToAnyThread: {
+                    LOGD("Parking current thread");
                     TRACE_EVENT("kernel", "YieldToAnyThread");
                     state.scheduler->ParkThread();
                     state.scheduler->WaitSchedule(false);
