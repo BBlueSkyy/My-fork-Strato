7cb91f93df2ab3909a80fbc98857444655ef6cc3
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 20:12:42 -0300

    am: expose process winding and shared applet identity

--- app/src/main/cpp/skyline/services/am/proxy/base_proxy.cpp
+++ app/src/main/cpp/skyline/services/am/proxy/base_proxy.cpp
@@ -6,51 +6,59 @@
 #include <services/am/controller/IWindowController.h>
 #include <services/am/controller/IAudioController.h>
 #include <services/am/controller/IDisplayController.h>
+#include <services/am/controller/IProcessWindingController.h>
 #include <services/am/controller/ILibraryAppletCreator.h>
 #include <services/am/controller/IDebugFunctions.h>
 #include <services/am/controller/IAppletCommonFunctions.h>
 #include "base_proxy.h"
 
 namespace skyline::service::am {
-    BaseProxy::BaseProxy(const DeviceState &state, ServiceManager &manager)
-        : BaseService(state, manager), appletState(std::make_shared<AppletState>(state)) {}
+    BaseProxy::BaseProxy(const DeviceState &state, ServiceManager &manager, u64 appletResourceUserId)
+        : BaseService(state, manager), appletState(std::make_shared<AppletState>(state)) {
+        appletState->appletResourceUserId = appletResourceUserId;
+    }
 
-    Result BaseProxy::GetCommonStateGetter(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetCommonStateGetter(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(ICommonStateGetter, appletState), session, response);
         return {};
     }
 
-    Result BaseProxy::GetSelfController(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetSelfController(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(ISelfController, appletState), session, response);
         return {};
     }
 
-    Result BaseProxy::GetWindowController(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetWindowController(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IWindowController, appletState), session, response);
         return {};
     }
 
-    Result BaseProxy::GetAudioController(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetAudioController(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IAudioController), session, response);
         return {};
     }
 
-    Result BaseProxy::GetDisplayController(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetDisplayController(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IDisplayController), session, response);
         return {};
     }
 
-    Result BaseProxy::GetLibraryAppletCreator(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetProcessWindingController(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
+        manager.RegisterService(SRVREG(IProcessWindingController, appletState), session, response);
+        return {};
+    }
+
+    Result BaseProxy::GetLibraryAppletCreator(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(ILibraryAppletCreator, appletState), session, response);
         return {};
     }
 
-    Result BaseProxy::GetDebugFunctions(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetDebugFunctions(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IDebugFunctions), session, response);
         return {};
     }
 
-    Result BaseProxy::GetAppletCommonFunctions(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result BaseProxy::GetAppletCommonFunctions(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IAppletCommonFunctions, appletState), session, response);
         return {};
     }
