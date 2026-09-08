defb6eac9f7082d9ad79028dd5e7a350127acec3
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 20:13:25 -0300

    am: modernize library applet proxy surface

--- app/src/main/cpp/skyline/services/am/proxy/ILibraryAppletProxy.h
+++ app/src/main/cpp/skyline/services/am/proxy/ILibraryAppletProxy.h
@@ -6,22 +6,20 @@
 #include "base_proxy.h"
 
 namespace skyline::service::am {
-    /**
-     * @brief ILibraryAppletProxy returns handles to various services
-     * @url https://switchbrew.org/wiki/Applet_Manager_services#ILibraryAppletProxy
-     */
     class ILibraryAppletProxy : public BaseProxy {
       public:
-        ILibraryAppletProxy(const DeviceState &state, ServiceManager &manager);
+        ILibraryAppletProxy(const DeviceState &state, ServiceManager &manager, u64 appletResourceUserId);
 
         SERVICE_DECL(
-            SFUNC(0x0, BaseProxy, GetCommonStateGetter),
-            SFUNC(0x1, BaseProxy, GetSelfController),
-            SFUNC(0x2, BaseProxy, GetWindowController),
-            SFUNC(0x3, BaseProxy, GetAudioController),
-            SFUNC(0x4, BaseProxy, GetDisplayController),
-            SFUNC(0xB, BaseProxy, GetLibraryAppletCreator),
-            SFUNC(0x3E8, BaseProxy, GetDebugFunctions)
+            SFUNC_BASE(0, ILibraryAppletProxy, BaseProxy, GetCommonStateGetter),
+            SFUNC_BASE(1, ILibraryAppletProxy, BaseProxy, GetSelfController),
+            SFUNC_BASE(2, ILibraryAppletProxy, BaseProxy, GetWindowController),
+            SFUNC_BASE(3, ILibraryAppletProxy, BaseProxy, GetAudioController),
+            SFUNC_BASE(4, ILibraryAppletProxy, BaseProxy, GetDisplayController),
+            SFUNC_BASE(10, ILibraryAppletProxy, BaseProxy, GetProcessWindingController),
+            SFUNC_BASE(11, ILibraryAppletProxy, BaseProxy, GetLibraryAppletCreator),
+            SFUNC_BASE(21, ILibraryAppletProxy, BaseProxy, GetAppletCommonFunctions),
+            SFUNC_BASE(1000, ILibraryAppletProxy, BaseProxy, GetDebugFunctions)
         )
     };
 }
