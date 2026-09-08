87bf7994075f277250528b7aff047827844c75a5
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 20:13:43 -0300

    am: modernize system applet proxy base surface

--- app/src/main/cpp/skyline/services/am/proxy/ISystemAppletProxy.h
+++ app/src/main/cpp/skyline/services/am/proxy/ISystemAppletProxy.h
@@ -6,23 +6,20 @@
 #include "base_proxy.h"
 
 namespace skyline::service::am {
-    /**
-     * @brief ISystemAppletProxy returns handles to various services
-     * @url https://switchbrew.org/wiki/Applet_Manager_services#ISystemAppletProxy
-     */
     class ISystemAppletProxy : public BaseProxy {
       public:
-        ISystemAppletProxy(const DeviceState &state, ServiceManager &manager);
+        ISystemAppletProxy(const DeviceState &state, ServiceManager &manager, u64 appletResourceUserId);
 
         SERVICE_DECL(
-            SFUNC(0x0, BaseProxy, GetCommonStateGetter),
-            SFUNC(0x1, BaseProxy, GetSelfController),
-            SFUNC(0x2, BaseProxy, GetWindowController),
-            SFUNC(0x3, BaseProxy, GetAudioController),
-            SFUNC(0x4, BaseProxy, GetDisplayController),
-            SFUNC(0xB, BaseProxy, GetLibraryAppletCreator),
-            SFUNC(0x17, BaseProxy, GetAppletCommonFunctions),
-            SFUNC(0x3E8, BaseProxy, GetDebugFunctions)
+            SFUNC_BASE(0, ISystemAppletProxy, BaseProxy, GetCommonStateGetter),
+            SFUNC_BASE(1, ISystemAppletProxy, BaseProxy, GetSelfController),
+            SFUNC_BASE(2, ISystemAppletProxy, BaseProxy, GetWindowController),
+            SFUNC_BASE(3, ISystemAppletProxy, BaseProxy, GetAudioController),
+            SFUNC_BASE(4, ISystemAppletProxy, BaseProxy, GetDisplayController),
+            SFUNC_BASE(10, ISystemAppletProxy, BaseProxy, GetProcessWindingController),
+            SFUNC_BASE(11, ISystemAppletProxy, BaseProxy, GetLibraryAppletCreator),
+            SFUNC_BASE(23, ISystemAppletProxy, BaseProxy, GetAppletCommonFunctions),
+            SFUNC_BASE(1000, ISystemAppletProxy, BaseProxy, GetDebugFunctions)
         )
     };
 }
