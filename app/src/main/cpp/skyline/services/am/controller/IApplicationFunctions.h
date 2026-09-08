92e97a85e485e439028fc05d3787e6804b2fec99
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 19:35:36 -0300

    am: modernize application functions surface

--- app/src/main/cpp/skyline/services/am/controller/IApplicationFunctions.h
+++ app/src/main/cpp/skyline/services/am/controller/IApplicationFunctions.h
@@ -3,8 +3,8 @@
 
 #pragma once
 
-#include <kernel/types/KEvent.h>
 #include <services/serviceman.h>
+#include <services/am/applet_state.h>
 
 namespace skyline::service::am {
     namespace result {
@@ -13,158 +13,63 @@ namespace skyline::service::am {
         constexpr Result InvalidParameters(128, 506);
     }
 
-    /**
-     * @brief This is used to notify an application about its own state
-     * @url https://switchbrew.org/wiki/Applet_Manager_services#IApplicationFunctions
-     */
     class IApplicationFunctions : public BaseService {
       private:
-        // Response values based on Ryujinx stub
-        // https://github.com/Ryujinx/Ryujinx/blob/b8556530f2b160db70ff571adf25ae26d4b8f58f/Ryujinx.HLE/HOS/Services/Am/AppletOE/ApplicationProxyService/ApplicationProxy/IApplicationFunctions.cs#L28
         static constexpr i64 SaveDataSize{200000000};
         static constexpr i64 JournalSaveDataSize{200000000};
-
-        std::shared_ptr<type::KEvent> gpuErrorEvent; //!< The event signalled on GPU errors
-        std::shared_ptr<type::KEvent> friendInvitationStorageChannelEvent; //!< The event signalled on friend invitations
-        std::shared_ptr<type::KEvent> notificationStorageChannelEvent;
-        std::shared_ptr<type::KEvent> unknownEvent210; //!< Stub event for cmd 210 (added in FW 20.0.0, unnamed on switchbrew), never signalled
-        i32 previousProgramIndex{-1}; //!< There was no previous title
+        std::shared_ptr<AppletState> appletState;
 
       public:
-        IApplicationFunctions(const DeviceState &state, ServiceManager &manager);
+        IApplicationFunctions(const DeviceState &state, ServiceManager &manager, std::shared_ptr<AppletState> appletState);
 
-        /**
-         * @brief Returns an Applet Manager IStorage containing the application's launch parameters
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#PopLaunchParameter
-         */
-        Result PopLaunchParameter(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Creates a save data folder for the requesting application
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#EnsureSaveData
-         */
-        Result EnsureSaveData(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Sets a termination result for the application
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetTerminateResult
-         */
-        Result SetTerminateResult(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the desired language for the application
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetDesiredLanguage
-         */
-        Result GetDesiredLanguage(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetDisplayVersion
-         */
-        Result GetDisplayVersion(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        Result GetSaveDataSize(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Ensures cache storage exists for the requesting application
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#IApplicationFunctions
-         * @note Real hardware allocates disk space and reports back the target media (NAND/SD) and
-         * required size; since we don't track real storage quotas, this stubs success and echoes
-         * back the requested sizes, following the same pattern already used by EnsureSaveData
-         */
-        Result CreateCacheStorage(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetSaveDataSizeMax
-         */
-        Result GetSaveDataSizeMax(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns if the application is running or not, always returns true
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#NotifyRunning
-         */
-        Result NotifyRunning(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns a V5 UUID generated from a seed in control.nacp and a device specific seed
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetPseudoDeviceId
-         */
-        Result GetPseudoDeviceId(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Initializes gameplay recording
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#InitializeGamePlayRecording
-         */
-        Result InitializeGamePlayRecording(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Sets the gameplay recording state
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetGamePlayRecordingState
-         */
-        Result SetGamePlayRecordingState(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#EnableApplicationCrashReport
-         */
-        Result EnableApplicationCrashReport(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Uses the given transfer memory to setup memory for the screenshot copyright image
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#InitializeApplicationCopyrightFrameBuffer
-         */
-        Result InitializeApplicationCopyrightFrameBuffer(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Sets the copyright image for screenshots using the buffer from InitializeApplicationCopyrightFrameBuffer
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetApplicationCopyrightImage
-         */
-        Result SetApplicationCopyrightImage(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Controls the visibility of the screenshot copyright image
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetApplicationCopyrightVisibility
-         */
-        Result SetApplicationCopyrightVisibility(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#QueryApplicationPlayStatistics
-         */
-        Result QueryApplicationPlayStatistics(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#QueryApplicationPlayStatisticsByUid
-         */
-        Result QueryApplicationPlayStatisticsByUid(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Gets the ProgramIndex of the Application which launched this title
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetPreviousProgramIndex
-         */
-        Result GetPreviousProgramIndex(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Obtains a handle to the system GPU error KEvent
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetGpuErrorDetectedSystemEvent
-         */
-        Result GetGpuErrorDetectedSystemEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetFriendInvitationStorageChannelEvent
-         */
-        Result GetFriendInvitationStorageChannelEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        Result TryPopFromFriendInvitationStorageChannel(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Obtains a handle to the Notification StorageChannel KEvent
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetNotificationStorageChannelEvent
-         */
-        Result GetNotificationStorageChannelEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Stub for an undocumented cmd added in FW 20.0.0 (no official name on switchbrew)
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#IApplicationFunctions
-         */
-        Result Cmd210(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result PopLaunchParameter(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result EnsureSaveData(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetDesiredLanguage(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetTerminateResult(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetDisplayVersion(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result ExtendSaveData(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetSaveDataSize(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result CreateCacheStorage(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetSaveDataSizeMax(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetCacheStorageMax(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result BeginBlockingHomeButtonShortAndLongPressed(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result EndBlockingHomeButtonShortAndLongPressed(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result BeginBlockingHomeButton(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result EndBlockingHomeButton(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetDeviceSaveDataSizeMax(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetLimitedApplicationLicenseUpgradableEvent(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result NotifyRunning(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetPseudoDeviceId(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetMediaPlaybackStateForApplication(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result IsGamePlayRecordingSupported(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result InitializeGamePlayRecording(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetGamePlayRecordingState(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result RequestFlushGamePlayingMovieForDebug(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result EnableApplicationCrashReport(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result InitializeApplicationCopyrightFrameBuffer(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetApplicationCopyrightImage(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetApplicationCopyrightVisibility(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result QueryApplicationPlayStatistics(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result QueryApplicationPlayStatisticsByUid(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result ClearUserChannel(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result UnpopToUserChannel(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetPreviousProgramIndex(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result EnableApplicationAllThreadDumpOnCrash(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetGpuErrorDetectedSystemEvent(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetDelayTimeToAbortOnGpuError(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetFriendInvitationStorageChannelEvent(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result TryPopFromFriendInvitationStorageChannel(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetNotificationStorageChannelEvent(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result TryPopFromNotificationStorageChannel(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetHealthWarningDisappearedSystemEvent(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetHdcpAuthenticationActivated(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetLaunchRequiredVersion(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result UpgradeLaunchRequiredVersion(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SendServerMaintenanceOverlayNotification(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result GetLastApplicationExitReason(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result Cmd210(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result SetAudioOutputPolicy(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
+        Result IsLanguageSelectionLimited(type::KSession &, ipc::IpcRequest &, ipc::IpcResponse &);
 
         SERVICE_DECL(
             SFUNC(0x1, IApplicationFunctions, PopLaunchParameter),
@@ -172,26 +77,49 @@ namespace skyline::service::am {
             SFUNC(0x15, IApplicationFunctions, GetDesiredLanguage),
             SFUNC(0x16, IApplicationFunctions, SetTerminateResult),
             SFUNC(0x17, IApplicationFunctions, GetDisplayVersion),
+            SFUNC(0x19, IApplicationFunctions, ExtendSaveData),
             SFUNC(0x1A, IApplicationFunctions, GetSaveDataSize),
             SFUNC(0x1B, IApplicationFunctions, CreateCacheStorage),
             SFUNC(0x1C, IApplicationFunctions, GetSaveDataSizeMax),
+            SFUNC(0x1D, IApplicationFunctions, GetCacheStorageMax),
+            SFUNC(0x1E, IApplicationFunctions, BeginBlockingHomeButtonShortAndLongPressed),
+            SFUNC(0x1F, IApplicationFunctions, EndBlockingHomeButtonShortAndLongPressed),
+            SFUNC(0x20, IApplicationFunctions, BeginBlockingHomeButton),
+            SFUNC(0x21, IApplicationFunctions, EndBlockingHomeButton),
+            SFUNC(0x23, IApplicationFunctions, GetDeviceSaveDataSizeMax),
+            SFUNC(0x25, IApplicationFunctions, GetLimitedApplicationLicenseUpgradableEvent),
             SFUNC(0x28, IApplicationFunctions, NotifyRunning),
             SFUNC(0x32, IApplicationFunctions, GetPseudoDeviceId),
+            SFUNC(0x3C, IApplicationFunctions, SetMediaPlaybackStateForApplication),
+            SFUNC(0x41, IApplicationFunctions, IsGamePlayRecordingSupported),
             SFUNC(0x42, IApplicationFunctions, InitializeGamePlayRecording),
             SFUNC(0x43, IApplicationFunctions, SetGamePlayRecordingState),
+            SFUNC(0x44, IApplicationFunctions, RequestFlushGamePlayingMovieForDebug),
             SFUNC(0x5A, IApplicationFunctions, EnableApplicationCrashReport),
             SFUNC(0x64, IApplicationFunctions, InitializeApplicationCopyrightFrameBuffer),
             SFUNC(0x65, IApplicationFunctions, SetApplicationCopyrightImage),
             SFUNC(0x66, IApplicationFunctions, SetApplicationCopyrightVisibility),
             SFUNC(0x6E, IApplicationFunctions, QueryApplicationPlayStatistics),
             SFUNC(0x6F, IApplicationFunctions, QueryApplicationPlayStatisticsByUid),
+            SFUNC(0x79, IApplicationFunctions, ClearUserChannel),
+            SFUNC(0x7A, IApplicationFunctions, UnpopToUserChannel),
             SFUNC(0x7B, IApplicationFunctions, GetPreviousProgramIndex),
+            SFUNC(0x7C, IApplicationFunctions, EnableApplicationAllThreadDumpOnCrash),
             SFUNC(0x82, IApplicationFunctions, GetGpuErrorDetectedSystemEvent),
+            SFUNC(0x83, IApplicationFunctions, SetDelayTimeToAbortOnGpuError),
             SFUNC(0x8C, IApplicationFunctions, GetFriendInvitationStorageChannelEvent),
             SFUNC(0x8D, IApplicationFunctions, TryPopFromFriendInvitationStorageChannel),
             SFUNC(0x96, IApplicationFunctions, GetNotificationStorageChannelEvent),
-            SFUNC(0xD2, IApplicationFunctions, Cmd210)
+            SFUNC(0x97, IApplicationFunctions, TryPopFromNotificationStorageChannel),
+            SFUNC(0xA0, IApplicationFunctions, GetHealthWarningDisappearedSystemEvent),
+            SFUNC(0xAA, IApplicationFunctions, SetHdcpAuthenticationActivated),
+            SFUNC(0xB4, IApplicationFunctions, GetLaunchRequiredVersion),
+            SFUNC(0xB5, IApplicationFunctions, UpgradeLaunchRequiredVersion),
+            SFUNC(0xBE, IApplicationFunctions, SendServerMaintenanceOverlayNotification),
+            SFUNC(0xC8, IApplicationFunctions, GetLastApplicationExitReason),
+            SFUNC(0xD2, IApplicationFunctions, Cmd210),
+            SFUNC(0xDC, IApplicationFunctions, SetAudioOutputPolicy),
+            SFUNC(0x14A, IApplicationFunctions, IsLanguageSelectionLimited)
         )
-
     };
 }
