38b1621c1140e2c1081b4db226215d992b68e9e1
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 19:31:42 -0300

    am: modernize common state getter lifecycle

--- app/src/main/cpp/skyline/services/am/controller/ICommonStateGetter.h
+++ app/src/main/cpp/skyline/services/am/controller/ICommonStateGetter.h
@@ -3,9 +3,9 @@
 
 #pragma once
 
-#include <deque>
 #include <kernel/types/KEvent.h>
 #include <services/serviceman.h>
+#include <services/am/applet_state.h>
 #include <common/macros.h>
 
 namespace skyline::service::am {
@@ -19,39 +19,32 @@ namespace skyline::service::am {
      */
     class ICommonStateGetter : public BaseService {
       private:
-        /**
-         * @brief All the possible contents of a #AppletMessage
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#AppletMessage
-         */
         enum class Message : u32 {
-            ExitRequested = 0x4,              //!< The applet has been requested to exit
-            FocusStateChange = 0xF,           //!< There was a change in the focus state of the applet
-            ExecutionResumed = 0x10,          //!< The execution of the applet has resumed
-            OperationModeChange = 0x1E,       //!< There was a change in the operation mode
-            PerformanceModeChange = 0x1F,     //!< There was a change in the performance mode
-            RequestToDisplay = 0x33,          //!< Indicates that ApproveToDisplay should be used
-            CaptureButtonShortPressed = 0x5A, //!< The Capture button was short pressed
-            ScreenshotTaken = 0x5C,           //!< A screenshot was taken
+            ExitRequested = 0x4,
+            FocusStateChange = 0xF,
+            ExecutionResumed = 0x10,
+            OperationModeChange = 0x1E,
+            PerformanceModeChange = 0x1F,
+            RequestToDisplay = 0x33,
+            CaptureButtonShortPressed = 0x5A,
+            ScreenshotTaken = 0x5C,
         };
 
-        std::shared_ptr<type::KEvent> messageEvent; //!< The event signalled when there is a message available
-        std::deque<Message> messageQueue; //!< A queue of all the messages that the program is yet to consume
-
         enum class FocusState : u8 {
-            InFocus = 1,    //!< The application is in foreground
-            OutOfFocus = 2, //!< The application is in the background
-        } focusState{FocusState::InFocus};
+            InFocus = 1,
+            OutOfFocus = 2,
+        };
 
         enum class OperationMode : u8 {
-            Handheld = 0, //!< The device is in handheld mode
-            Docked = 1,   //!< The device is in docked mode
-        } operationMode;
+            Handheld = 0,
+            Docked = 1,
+        };
 
         enum class CpuBoostMode : u32 {
-            Normal = 0,     //!< The device runs at stock CPU and CPU clocks
-            FastLoad = 1,   //!< The device runs at boosted CPU clocks and minimum GPU clocks
-            PowerSaving = 2 //!< The device runs at stock CPU clocks and minimum GPU clocks
-        } cpuBoostMode;
+            Normal = 0,
+            FastLoad = 1,
+            PowerSaving = 2
+        };
 
         ENUM_STRING(CpuBoostMode, {
             ENUM_CASE_PAIR(Normal, "Normal");
@@ -59,95 +52,40 @@ namespace skyline::service::am {
             ENUM_CASE_PAIR(PowerSaving, "Power Saving");
         })
 
-        std::shared_ptr<type::KEvent> defaultDisplayResolutionChangeEvent; //!< Signalled when the default display resolution changes,
-
-        bool vrModeEnabled{false};
-
-        /**
-         * @brief Queues a message for the application to read via ReceiveMessage
-         * @param message The message to queue
-         */
-        void QueueMessage(Message message);
+        std::shared_ptr<AppletState> appletState;
 
       public:
-        ICommonStateGetter(const DeviceState &state, ServiceManager &manager);
+        ICommonStateGetter(const DeviceState &state, ServiceManager &manager, std::shared_ptr<AppletState> appletState);
 
-        /**
-         * @brief Returns the handle to a KEvent object that is signalled whenever RecieveMessage has a message
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetEventHandle
-         */
         Result GetEventHandle(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns an #AppletMessage or 0x680 to indicate the lack of a message
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#ReceiveMessage
-         */
         Result ReceiveMessage(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns if an application is in focus or not. It always returns in focus on the emulator
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetCurrentFocusState
-         */
-        Result GetCurrentFocusState(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the current OperationMode
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetOperationMode
-         */
         Result GetOperationMode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the current PerformanceMode (Same as operationMode but u32)
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetPerformanceMode
-         */
         Result GetPerformanceMode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the state of VR mode
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#IsVrModeEnabled
-         */
+        Result GetBootMode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetCurrentFocusState(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result RequestToAcquireSleepLock(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result ReleaseSleepLock(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result ReleaseSleepLockTransiently(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetAcquiredSleepLockEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetWakeupCount(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result IsVrModeEnabled(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetVrModeEnabled
-         */
         Result SetVrModeEnabled(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetLcdBacklighOffEnabled
-         */
         Result SetLcdBacklighOffEnabled(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#BeginVrModeEx
-         */
         Result BeginVrModeEx(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#EndVrModeEx
-         */
         Result EndVrModeEx(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the current display width and height in two u32s
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetDefaultDisplayResolution
-         */
+        Result IsInControllerFirmwareUpdateSection(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result GetDefaultDisplayResolution(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#GetDefaultDisplayResolutionChangeEvent
-         */
         Result GetDefaultDisplayResolutionChangeEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Sets the CPU boost mode to the supplied value
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetCpuBoostMode
-         */
+        Result GetHdcpAuthenticationState(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetHdcpAuthenticationStateChangeEvent(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result SetCpuBoostMode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Applet_Manager_services#SetRequestExitToLibraryAppletAtExecuteNextProgramEnabled
-         */
+        Result CancelCpuBoostMode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result GetBuiltInDisplayType(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result IsSleepEnabled(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result IsDisablingSleepSuppressed(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result BeginVrMode3d(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result EndVrMode3d(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result IsVrModeEnabled3d(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result SetRequestExitToLibraryAppletAtExecuteNextProgramEnabled(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
@@ -155,16 +93,32 @@ namespace skyline::service::am {
             SFUNC(0x1, ICommonStateGetter, ReceiveMessage),
             SFUNC(0x5, ICommonStateGetter, GetOperationMode),
             SFUNC(0x6, ICommonStateGetter, GetPerformanceMode),
+            SFUNC(0x8, ICommonStateGetter, GetBootMode),
             SFUNC(0x9, ICommonStateGetter, GetCurrentFocusState),
+            SFUNC(0xA, ICommonStateGetter, RequestToAcquireSleepLock),
+            SFUNC(0xB, ICommonStateGetter, ReleaseSleepLock),
+            SFUNC(0xC, ICommonStateGetter, ReleaseSleepLockTransiently),
+            SFUNC(0xD, ICommonStateGetter, GetAcquiredSleepLockEvent),
+            SFUNC(0xE, ICommonStateGetter, GetWakeupCount),
             SFUNC(0x32, ICommonStateGetter, IsVrModeEnabled),
             SFUNC(0x33, ICommonStateGetter, SetVrModeEnabled),
             SFUNC(0x34, ICommonStateGetter, SetLcdBacklighOffEnabled),
             SFUNC(0x35, ICommonStateGetter, BeginVrModeEx),
             SFUNC(0x36, ICommonStateGetter, EndVrModeEx),
+            SFUNC(0x37, ICommonStateGetter, IsInControllerFirmwareUpdateSection),
             SFUNC(0x3C, ICommonStateGetter, GetDefaultDisplayResolution),
             SFUNC(0x3D, ICommonStateGetter, GetDefaultDisplayResolutionChangeEvent),
+            SFUNC(0x3E, ICommonStateGetter, GetHdcpAuthenticationState),
+            SFUNC(0x3F, ICommonStateGetter, GetHdcpAuthenticationStateChangeEvent),
             SFUNC(0x42, ICommonStateGetter, SetCpuBoostMode),
-            SFUNC(0x384, ICommonStateGetter, SetRequestExitToLibraryAppletAtExecuteNextProgramEnabled)
+            SFUNC(0x43, ICommonStateGetter, CancelCpuBoostMode),
+            SFUNC(0x44, ICommonStateGetter, GetBuiltInDisplayType),
+            SFUNC(0x1F6, ICommonStateGetter, IsSleepEnabled),
+            SFUNC(0x1F7, ICommonStateGetter, IsDisablingSleepSuppressed),
+            SFUNC(0x384, ICommonStateGetter, SetRequestExitToLibraryAppletAtExecuteNextProgramEnabled),
+            SFUNC(0x3E8, ICommonStateGetter, BeginVrMode3d),
+            SFUNC(0x3E9, ICommonStateGetter, EndVrMode3d),
+            SFUNC(0x3EA, ICommonStateGetter, IsVrModeEnabled3d)
         )
     };
 }
