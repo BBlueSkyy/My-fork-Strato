3def47c2d31b495180bbb24afab3af11aa48d66c
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-08 20:13:07 -0300

    am: bind application proxy to client aruid

--- app/src/main/cpp/skyline/services/am/proxy/IApplicationProxy.cpp
+++ app/src/main/cpp/skyline/services/am/proxy/IApplicationProxy.cpp
@@ -5,9 +5,10 @@
 #include "IApplicationProxy.h"
 
 namespace skyline::service::am {
-    IApplicationProxy::IApplicationProxy(const DeviceState &state, ServiceManager &manager) : BaseProxy(state, manager) {}
+    IApplicationProxy::IApplicationProxy(const DeviceState &state, ServiceManager &manager, u64 appletResourceUserId)
+        : BaseProxy(state, manager, appletResourceUserId) {}
 
-    Result IApplicationProxy::GetApplicationFunctions(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+    Result IApplicationProxy::GetApplicationFunctions(type::KSession &session, ipc::IpcRequest &, ipc::IpcResponse &response) {
         manager.RegisterService(SRVREG(IApplicationFunctions, appletState), session, response);
         return {};
     }
