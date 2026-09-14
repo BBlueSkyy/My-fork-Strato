7d44fe5cba83a2bbd092096e6d78bc838361d027
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-14 12:25:00 -0300

    nifm: model IRequest state transitions

--- app/src/main/cpp/skyline/services/nifm/IRequest.h
+++ app/src/main/cpp/skyline/services/nifm/IRequest.h
@@ -24,50 +24,21 @@ namespace skyline::service::nifm {
      */
     class IRequest : public BaseService {
       private:
-        std::shared_ptr<type::KEvent> event0; //!< The KEvent that is signalled on request state changes
-        std::shared_ptr<type::KEvent> event1; //!< The KEvent that is signalled on request changes
-        RequestState requestState; //!< The current state of this request, returned by GetRequestState
+        std::shared_ptr<type::KEvent> event0; //!< Signals when RequestState changes
+        std::shared_ptr<type::KEvent> event1; //!< Secondary request-change event
+        RequestState requestState{RequestState::Free};
+
+        void UpdateState(RequestState newState);
 
       public:
         IRequest(const DeviceState &state, ServiceManager &manager);
 
-        /**
-         * @brief Returns the current state of the request
-         * @url https://switchbrew.org/wiki/Network_Interface_services#GetRequestState
-         */
         Result GetRequestState(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the error code if a network bring up request fails
-         * @url https://switchbrew.org/wiki/Network_Interface_services#GetResult
-         */
         Result GetResult(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns two KEvent handles that signal request on request updates
-         * @url https://switchbrew.org/wiki/Network_Interface_services#GetSystemEventReadableHandles
-         */
         Result GetSystemEventReadableHandles(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Network_Interface_services#Cancel
-         */
         Result Cancel(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Submits a request to bring up a network
-         * @url https://switchbrew.org/wiki/Network_Interface_services#Submit
-         */
         Result Submit(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Network_Interface_services#SetConnectionConfirmationOption
-         */
         Result SetConnectionConfirmationOption(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @url https://switchbrew.org/wiki/Network_Interface_services#GetAppletInfo
-         */
         Result GetAppletInfo(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
