9a19f6fc98ac842b3d8a0fcef8753c0e01cfd698
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-14 12:25:23 -0300

    nifm: separate static service ABI variants

--- app/src/main/cpp/skyline/services/nifm/IStaticService.h
+++ app/src/main/cpp/skyline/services/nifm/IStaticService.h
@@ -7,21 +7,18 @@
 
 namespace skyline::service::nifm {
     /**
-     * @brief IStaticService or nifm:u is used by applications to open an IGeneralService instance for controlling the network connection
-     * @url https://switchbrew.org/wiki/Network_Interface_services#IGeneralService
+     * @brief nn::nifm::detail::IStaticService exposed as nifm:u/nifm:s/nifm:a
+     * @url https://switchbrew.org/wiki/Network_Interface_services
      */
     class IStaticService : public BaseService {
       public:
         IStaticService(const DeviceState &state, ServiceManager &manager);
 
-        /**
-         * @brief Opens an IGeneralService that can be used by applications to control the network connection
-         * @url https://switchbrew.org/wiki/Network_Interface_services#CreateGeneralServiceOld
-         */
+        Result CreateGeneralServiceOld(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result CreateGeneralService(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
-            SFUNC(0x4, IStaticService, CreateGeneralService),
+            SFUNC(0x4, IStaticService, CreateGeneralServiceOld),
             SFUNC(0x5, IStaticService, CreateGeneralService)
         )
     };
