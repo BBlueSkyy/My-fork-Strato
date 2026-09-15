12bc3120e242f4936fbbb776b9725565f72c89ce
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-14 21:18:47 -0300

    account: modernize baas manager ABI

--- app/src/main/cpp/skyline/services/account/IManagerForApplication.h
+++ app/src/main/cpp/skyline/services/account/IManagerForApplication.h
@@ -7,46 +7,35 @@
 #include "IAccountServiceForApplication.h"
 
 namespace skyline::service::account {
-    /**
-    * @brief IManagerForApplication provides functions for reading Nintendo Online user information
-    * @url https://switchbrew.org/wiki/Account_services#IManagerForApplication
-    */
     class IManagerForApplication : public BaseService {
       private:
+        static constexpr size_t NasUserBaseForApplicationSize{0x68};
+        static constexpr size_t ModernIdTokenCacheSize{0x100};
+
+        UserId userId;
         std::shared_ptr<std::vector<UserId>> openedUsers;
 
+        u64 GetNetworkServiceAccountId() const;
+        Result WriteIdTokenCache(ipc::IpcRequest &request, ipc::IpcResponse &response, size_t size);
+
       public:
-        IManagerForApplication(const DeviceState &state, ServiceManager &manager, std::vector<UserId> &openedUsers);
+        IManagerForApplication(const DeviceState &state, ServiceManager &manager, UserId userId, std::shared_ptr<std::vector<UserId>> openedUsers);
 
-        /**
-        * @brief This checks if the given user has access to online services
-        */
         Result CheckAvailability(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        /**
-         * @brief Returns the user ID of the current user
-         */
         Result GetAccountId(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
         Result EnsureIdTokenCacheAsync(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
+        Result LoadIdTokenCacheDeprecated(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
         Result LoadIdTokenCache(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        Result CreateAuthorizationRequest(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-
-        Result StoreOpenContext(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
-      
-        /**
-         * @brief Returns the cached Nintendo Account resource for this user
-         * @url https://switchbrew.org/wiki/Account_services#GetNintendoAccountUserResourceCacheForApplication
-         */
         Result GetNintendoAccountUserResourceCacheForApplication(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result CreateAuthorizationRequest(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+        Result StoreOpenContext(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
             SFUNC(0x0, IManagerForApplication, CheckAvailability),
             SFUNC(0x1, IManagerForApplication, GetAccountId),
             SFUNC(0x2, IManagerForApplication, EnsureIdTokenCacheAsync),
-            SFUNC(0x3, IManagerForApplication, LoadIdTokenCache),
+            SFUNC(0x3, IManagerForApplication, LoadIdTokenCacheDeprecated),
+            SFUNC(0x4, IManagerForApplication, LoadIdTokenCache),
             SFUNC(0x82, IManagerForApplication, GetNintendoAccountUserResourceCacheForApplication),
             SFUNC(0x96, IManagerForApplication, CreateAuthorizationRequest),
             SFUNC(0xA0, IManagerForApplication, StoreOpenContext)
