0ff4678b127041d627ce079c137b411f7e247ad2
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-14 21:20:08 -0300

    account: expose unauthorised state without fake tokens

--- app/src/main/cpp/skyline/services/account/IAuthorizationRequest.cpp
+++ app/src/main/cpp/skyline/services/account/IAuthorizationRequest.cpp
@@ -2,22 +2,13 @@
 // Copyright © 2023 Skyline Team and Contributors (https://github.com/skyline-emu/)
 
 #include "IAuthorizationRequest.h"
-#include "IAsyncContext.h"
 
 namespace skyline::service::account {
     IAuthorizationRequest::IAuthorizationRequest(const DeviceState &state, ServiceManager &manager) : BaseService(state, manager) {}
 
-    Result IAuthorizationRequest::InvokeWithoutInteractionAsync(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        manager.RegisterService(SRVREG(IAsyncContext), session, response);
-        return {};
-    }
-
     Result IAuthorizationRequest::IsAuthorized(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        response.Push<u8>(0);
-        return {};
-    }
-
-    Result IAuthorizationRequest::GetAuthorizationCode(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+        // Strato has no Nintendo authorization backend, so expose the real local state instead of a fake token/code.
+        response.Push<u8>(false);
         return {};
     }
 }
