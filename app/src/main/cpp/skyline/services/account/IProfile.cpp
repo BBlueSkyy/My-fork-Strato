76f78bb3608fa8ac2914b0cebd96b2b3dd46caed
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-14 21:19:50 -0300

    account: validate profile buffers and layouts

--- app/src/main/cpp/skyline/services/account/IProfile.cpp
+++ app/src/main/cpp/skyline/services/account/IProfile.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright © 2020 Skyline Team and Contributors (https://github.com/skyline-emu/)
 
+#include <cstring>
 #include <os.h>
 #include <vfs/os_backing.h>
 #include <fcntl.h>
@@ -8,63 +9,87 @@
 #include "IProfile.h"
 
 namespace skyline::service::account {
+    namespace {
+        struct AccountUserData {
+            u32 unknown{};
+            u32 iconId{};
+            u8 iconBackgroundColorId{};
+            std::array<u8, 0x7> reserved0{};
+            std::array<u8, 0x10> miiId{};
+            std::array<u8, 0x60> reserved1{};
+        };
+        static_assert(sizeof(AccountUserData) == 0x80);
+
+        struct AccountProfileBase {
+            UserId uid{};
+            u64 lastEditTimestamp{};
+            std::array<char, 0x20> nickname{};
+        };
+        static_assert(sizeof(AccountProfileBase) == 0x38);
+    }
+
     IProfile::IProfile(const DeviceState &state, ServiceManager &manager, const UserId &userId) : userId(userId), BaseService(state, manager) {}
 
     Result IProfile::Get(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        struct AccountUserData {
-            u32 _unk0_;
-            u32 iconID;                        //!< Icon ID (0 = Mii, the rest are character icon IDs)
-            u8 iconBackgroundColorID;          //!< Profile icon background color ID
-            u8 _unk1_[0x7];
-            std::array<u8, 0x10> miiID;        //!< Some ID related to the Mii? All zeros when a character icon is used
-            u8 _unk2_[0x60];
-        };
+        span<u8> output;
+        try {
+            output = request.outputBuf.at(0);
+        } catch (const std::out_of_range &) {
+            return result::InvalidInputBuffer;
+        }
 
-        request.outputBuf.at(0).as<AccountUserData>().iconBackgroundColorID = 0x1; // Color indexing starts at 0x1
+        if (output.size() < sizeof(AccountUserData))
+            return result::InvalidBufferSize;
+
+        auto &userData{output.as<AccountUserData>()};
+        userData = {};
+        userData.iconId = 1;
+        userData.iconBackgroundColorId = 1;
 
         return GetBase(session, request, response);
     }
 
     Result IProfile::GetBase(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        struct {
-            UserId uid;                        //!< The UID of the corresponding account
-            u64 lastEditTimestamp;             //!< A POSIX UTC timestamp denoting the last account edit
-            std::array<char, 0x20> nickname;   //!< UTF-8 Nickname
-        } accountProfileBase = {
+        AccountProfileBase accountProfileBase{
             .uid = userId,
         };
 
-        size_t usernameSize{std::min(accountProfileBase.nickname.size() - 1, (*state.settings->usernameValue).size())};
-        std::memcpy(accountProfileBase.nickname.data(), (*state.settings->usernameValue).c_str(), usernameSize);
+        const size_t usernameSize{std::min(accountProfileBase.nickname.size() - 1, (*state.settings->usernameValue).size())};
+        std::memcpy(accountProfileBase.nickname.data(), (*state.settings->usernameValue).data(), usernameSize);
 
         response.Push(accountProfileBase);
-
         return {};
     }
 
     Result IProfile::GetImageSize(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        std::shared_ptr<vfs::Backing> profileImageIcon{GetProfilePicture()};
+        const auto profileImageIcon{GetProfilePicture()};
         response.Push(static_cast<u32>(profileImageIcon->size));
-
         return {};
     }
 
     Result IProfile::LoadImage(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
-        std::shared_ptr<vfs::Backing> profileImageIcon{GetProfilePicture()};
+        const auto profileImageIcon{GetProfilePicture()};
 
-        profileImageIcon->Read(span(request.outputBuf.at(0)).first(profileImageIcon->size), 0);
+        span<u8> output;
+        try {
+            output = request.outputBuf.at(0);
+        } catch (const std::out_of_range &) {
+            return result::InvalidInputBuffer;
+        }
+
+        if (output.size() < profileImageIcon->size)
+            return result::InvalidBufferSize;
+
+        profileImageIcon->Read(output.first(profileImageIcon->size), 0);
         response.Push(static_cast<u32>(profileImageIcon->size));
-
         return {};
     }
 
     std::shared_ptr<vfs::Backing> IProfile::GetProfilePicture() {
-        std::string profilePicturePath{*state.settings->profilePictureValue};
-        int fd{open((profilePicturePath).c_str(), O_RDONLY)};
+        const std::string profilePicturePath{*state.settings->profilePictureValue};
+        const int fd{open(profilePicturePath.c_str(), O_RDONLY)};
         if (fd < 0)
-            // If we can't find the profile picture then just return the placeholder profile picture
             return state.os->assetFileSystem->OpenFile("profile_picture.jpeg");
-        else
-            return std::make_shared<vfs::OsBacking>(fd, true);
+        return std::make_shared<vfs::OsBacking>(fd, true);
     }
 }
