a8c156a56fa0ed6e5292d9b67647b0367aaa2bc0
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-16 09:16:34 -0300

    swkbd: implement inline HLE frontend state

--- app/src/main/cpp/skyline/applet/swkbd/software_keyboard_applet.h
+++ app/src/main/cpp/skyline/applet/swkbd/software_keyboard_applet.h
@@ -3,46 +3,37 @@
 
 #pragma once
 
+#include <future>
 #include <services/am/applet/IApplet.h>
 #include <services/applet/common_arguments.h>
 #include <jvm.h>
 #include "software_keyboard_config.h"
+#include "software_keyboard_inline.h"
 
 namespace skyline::applet::swkbd {
     static_assert(sizeof(KeyboardConfigVB) == sizeof(JvmManager::KeyboardConfig));
 
     /**
      * @url https://switchbrew.org/wiki/Software_Keyboard
-     * @brief An implementation for the Software Keyboard (swkbd) Applet which handles translating guest applet transactions to the appropriate host behavior
+     * @brief Translates Software Keyboard applet transactions to the Android frontend.
      */
     class SoftwareKeyboardApplet : public service::am::IApplet, service::am::EnableNormalQueue {
       private:
-        /**
-         * @url https://switchbrew.org/wiki/Software_Keyboard#CloseResult
-         */
         enum class CloseResult : u32 {
             Enter = 0x0,
             Cancel = 0x1,
         };
 
-        /**
-         * @url https://switchbrew.org/wiki/Software_Keyboard#TextCheckResult
-         */
         enum class TextCheckResult : u32 {
             Success = 0x0,
             ShowFailureDialog = 0x1,
             ShowConfirmDialog = 0x2,
         };
 
-        static constexpr u32 SwkbdTextBytes{0x7D4}; //!< Size of the returned IStorage buffer that's used to return the input text
+        static constexpr u32 SwkbdTextBytes{0x7D4};
+        static constexpr u32 MaxOneLineChars{32};
 
-        static constexpr u32 MaxOneLineChars{32}; //!< The maximum number of characters for which anything other than InputFormMode::MultiLine is used
-
-        #pragma pack(push, 1)
-
-        /**
-         * @brief The final result after the swkbd has closed
-         */
+#pragma pack(push, 1)
         struct OutputResult {
             CloseResult closeResult;
             std::array<u8, SwkbdTextBytes> chars{};
@@ -51,9 +42,6 @@ namespace skyline::applet::swkbd {
         };
         static_assert(sizeof(OutputResult) == 0x7D8);
 
-        /**
-         * @brief A request for validating a string inside guest code, this is pushed via the interactive queue
-         */
         struct ValidationRequest {
             u64 size;
             std::array<u8, SwkbdTextBytes> chars{};
@@ -62,36 +50,66 @@ namespace skyline::applet::swkbd {
         };
         static_assert(sizeof(ValidationRequest) == 0x7DC);
 
-        /**
-         * @brief The result of validating text submitted to the guest
-         */
         struct ValidationResult {
             TextCheckResult result;
             std::array<char16_t, SwkbdTextBytes / sizeof(char16_t)> chars;
         };
         static_assert(sizeof(ValidationResult) == 0x7D8);
-
-        #pragma pack(pop)
+#pragma pack(pop)
 
         KeyboardConfigVB config{};
         service::applet::LibraryAppletMode mode{};
         bool validationPending{};
         std::u16string currentText{};
         CloseResult currentResult{};
-
         jobject dialog{};
+
+        std::mutex inlineMutex;
+        std::future<void> inlineInputFuture;
+        JvmManager::KeyboardHandle pendingInlineWaitDialog{};
+        inline_protocol::InitializeArg inlineInitializeArg{};
+        inline_protocol::CalcArgCommon inlineCalcCommon{};
+        inline_protocol::CalcArgOldBody inlineCalcOld{};
+        inline_protocol::CalcArgNewBody inlineCalcNew{};
+        inline_protocol::State inlineState{inline_protocol::State::NotInitialized};
+        bool inlineStarted{};
+        bool inlineUseUtf8{};
+        bool inlineUseChangedStringV2{};
+        bool inlineUseMovedCursorV2{};
+        bool inlineUsesNewLayout{};
+        i32 inlineCursorPosition{};
+        std::shared_ptr<service::am::IStorage> inlineDictionaryStorage{};
 
         void SendResult();
 
+        Result StartInline();
+        void ProcessInlineStorage(std::shared_ptr<service::am::IStorage> data);
+        void ProcessInlineRequest(span<u8> data);
+        void ProcessInlineCalc(span<u8> data);
+        void ProcessInlineCalcOld();
+        void ProcessInlineCalcNew();
+        void ConfigureInlineKeyboardOld();
+        void ConfigureInlineKeyboardNew();
+
+        void ChangeInlineState(inline_protocol::State state);
+        void ShowInlineKeyboard();
+        void HideInlineKeyboard();
+        void WaitForInlineKeyboardInput(JvmManager::KeyboardHandle workerDialog);
+
+        void SendInlineReply(inline_protocol::Reply reply, span<const u8> payload = {});
+        void SendInlineTextReply(inline_protocol::Reply reply, std::u16string_view text, i32 cursor = 0);
+
       public:
-        SoftwareKeyboardApplet(const DeviceState &state, service::ServiceManager &manager, std::shared_ptr<kernel::type::KEvent> onAppletStateChanged, std::shared_ptr<kernel::type::KEvent> onNormalDataPushFromApplet, std::shared_ptr<kernel::type::KEvent> onInteractiveDataPushFromApplet, service::applet::LibraryAppletMode appletMode);
+        SoftwareKeyboardApplet(const DeviceState &state, service::ServiceManager &manager,
+                               std::shared_ptr<kernel::type::KEvent> onAppletStateChanged,
+                               std::shared_ptr<kernel::type::KEvent> onNormalDataPushFromApplet,
+                               std::shared_ptr<kernel::type::KEvent> onInteractiveDataPushFromApplet,
+                               service::applet::LibraryAppletMode appletMode);
+        ~SoftwareKeyboardApplet() override;
 
         Result Start() override;
-
         Result GetResult() override;
-
         void PushNormalDataToApplet(std::shared_ptr<service::am::IStorage> data) override;
-
         void PushInteractiveDataToApplet(std::shared_ptr<service::am::IStorage> data) override;
     };
 }
