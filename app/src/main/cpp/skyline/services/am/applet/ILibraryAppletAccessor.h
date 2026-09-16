e6a02f552360d2cac4a5b2931477485a45e9f65c
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-16 09:21:56 -0300

    am: provide inline applet consumer handle

--- app/src/main/cpp/skyline/services/am/applet/ILibraryAppletAccessor.h
+++ app/src/main/cpp/skyline/services/am/applet/ILibraryAppletAccessor.h
@@ -29,6 +29,8 @@ namespace skyline::service::am {
         skyline::applet::AppletId appletId;
         applet::LibraryAppletMode appletMode;
         std::shared_ptr<IApplet> applet;
+        u64 indirectLayerHandle{};
+        u64 indirectLayerAruid{};
 
       public:
         ILibraryAppletAccessor(const DeviceState &state, ServiceManager &manager,
