3edf6dd29b2970955b9f211d82e271823445bf45
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-23 21:25:39 -0300

    debug: remove per-method GRID logging

--- app/src/main/cpp/skyline/soc/gm20b/engines/gpfifo.cpp
+++ app/src/main/cpp/skyline/soc/gm20b/engines/gpfifo.cpp
@@ -13,13 +13,7 @@ namespace skyline::soc::gm20b::engine {
     void GPFIFO::CallMethod(u32 method, u32 argument) {
         LOGD("Called method in GPFIFO: 0x{:X} args: 0x{:X}", method, argument);
 
-        if (method == 0xB)
-            LOGI("GRID-MEMOPB gpfifo-call-begin argument=0x{:X}", argument);
-
         registers.raw[method] = argument;
-
-        if (method == 0xB)
-            LOGI("GRID-MEMOPB register-write-end");
 
         switch (method) {
             ENGINE_STRUCT_CASE(syncpoint, action, {
@@ -131,7 +125,5 @@ namespace skyline::soc::gm20b::engine {
             })
         }
 
-        if (method == 0xB)
-            LOGI("GRID-MEMOPB gpfifo-call-end");
     };
 }
